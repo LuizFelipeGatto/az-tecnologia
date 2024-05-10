b4485c5c5bf6ba0b20fff2406161076525eaeebf
luiz.felipe <luiz.felipe@geotech4web.com.br>
2024-05-10 15:48:29 -0300

    :fix exercício 2 - populando banco

---
+++ backend/src/main/resources/db/migration/seeds/V100__POPULANDO_TABELAS.sql
@@ -0,0 +1,495 @@
+-- Dados na tabela Empresa
+INSERT INTO projeto.empresa VALUES (
+       DEFAULT,
+       'Empresa Delta Ltda',
+       '13579246802468',
+       'Rua das Orquídeas',
+       'Porto Alegre',
+       '404',
+       'Sala 202',
+       'Centro',
+       '24680-135',
+       '(51) 2468-1357',
+       'contato@empresadelta.com',
+       'http://www.empresadelta.com',
+       'delta_user',
+       'delta_pass',
+       CURRENT_TIMESTAMP,
+       CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.empresa VALUES (
+       DEFAULT,
+       'Empresa Epsilon S/A',
+       '24681357913579',
+       'Avenida dos Lírios',
+       'Curitiba',
+       '505',
+       'Andar 5',
+       'Batel',
+       '35791-246',
+       '(41) 3579-1246',
+       'contato@empresaepsilon.com',
+       'http://www.empresaepsilon.com',
+       'epsilon_user',
+       'epsilon_pass',
+       CURRENT_TIMESTAMP,
+       CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.empresa VALUES (
+       DEFAULT,
+       'Empresa Zeta & Filhos',
+       '98765432101234',
+       'Rua das Tulipas',
+       'Brasília',
+       '606',
+       'Sala 606',
+       'Asa Norte',
+       '01234-567',
+       '(61) 0123-4567',
+       'contato@empresazeta.com',
+       'http://www.empresazeta.com',
+       'zeta_user',
+       'zeta_pass',
+       CURRENT_TIMESTAMP,
+       CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.empresa VALUES (
+       DEFAULT,
+       'Empresa Theta Ltda',
+       '12309876543210',
+       'Avenida das Margaridas',
+       'Fortaleza',
+       '707',
+       'Sala 1010',
+       'Aldeota',
+       '98765-432',
+       '(85) 9876-5432',
+       'contato@empresatheta.com',
+       'http://www.empresatheta.com',
+       'theta_user',
+       'theta_pass',
+       CURRENT_TIMESTAMP,
+       CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.empresa VALUES (
+       DEFAULT,
+       'Empresa Iota & Cia',
+       '78901234567890',
+       'Rua dos Cravos',
+       'Recife',
+       '808',
+       'Andar 8',
+       'Boa Viagem',
+       '34567-890',
+       '(81) 3456-7890',
+       'contato@empresaiota.com',
+       'http://www.empresaiota.com',
+       'iota_user',
+       'iota_pass',
+       CURRENT_TIMESTAMP,
+       CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.empresa VALUES (
+       DEFAULT,
+       'Empresa Kappa Ltda',
+       '54321678901234',
+       'Avenida das Bromélias',
+       'Manaus',
+       '909',
+       'Sala 909',
+       'Centro',
+       '67890-123',
+       '(92) 6789-0123',
+       'contato@empresakappa.com',
+       'http://www.empresakappa.com',
+       'kappa_user',
+       'kappa_pass',
+       CURRENT_TIMESTAMP,
+       CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.empresa VALUES (
+       DEFAULT,
+       'Empresa Lambda S/A',
+       '01234567898765',
+       'Rua das Hortênsias',
+       'Salvador',
+       '1010',
+       'Andar 10',
+       'Pituba',
+       '56789-012',
+       '(71) 5678-9012',
+       'contato@empresalambda.com',
+       'http://www.empresalambda.com',
+       'lambda_user',
+       'lambda_pass',
+       CURRENT_TIMESTAMP,
+       CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.empresa VALUES (
+       DEFAULT,
+       'Empresa Mu & Filhos',
+       '98701234567890',
+       'Avenida das Gérberas',
+       'Goiânia',
+       '1111',
+       'Sala 1111',
+       'Setor Bueno',
+       '01234-567',
+       '(62) 0123-4567',
+       'contato@empresamu.com',
+       'http://www.empresamu.com',
+       'mu_user',
+       'mu_pass',
+       CURRENT_TIMESTAMP,
+       CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.empresa VALUES (
+       DEFAULT,
+       'Empresa Nu Ltda',
+       '01234987654321',
+       'Rua das Begônias',
+       'Natal',
+       '1212',
+       'Andar 12',
+       'Ponta Negra',
+       '98765-432',
+       '(84) 9876-5432',
+       'contato@empresanu.com',
+       'http://www.empresanu.com',
+       'nu_user',
+       'nu_pass',
+       CURRENT_TIMESTAMP,
+       CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.empresa VALUES (
+       DEFAULT,
+       'Empresa Xi S/A',
+       '98765012345678',
+       'Avenida das Azaléias',
+       'Florianópolis',
+       '1313',
+       'Andar 13',
+       'Centro',
+       '01234-567',
+       '(48) 0123-4567',
+       'contato@empresaxi.com',
+       'http://www.empresaxi.com',
+       'xi_user',
+       'xi_pass',
+       CURRENT_TIMESTAMP,
+       CURRENT_TIMESTAMP
+);
+
+-- Dados na tabela Leilão
+INSERT INTO projeto.leilao VALUES (
+      DEFAULT,
+      101,
+      'Leilão de jóias',
+      3,
+      '2024-05-20 14:00:00',
+      CURRENT_TIMESTAMP,
+      CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.leilao VALUES (
+      DEFAULT,
+      202,
+      'Leilão de carros antigos',
+      2,
+      '2024-05-25 09:30:00',
+      CURRENT_TIMESTAMP,
+      CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.leilao VALUES (
+      DEFAULT,
+      303,
+      'Leilão de obras de arte contemporânea',
+      5,
+      '2024-06-01 15:00:00',
+      CURRENT_TIMESTAMP,
+      CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.leilao VALUES (
+      DEFAULT,
+      404,
+      'Leilão de instrumentos musicais raros',
+      4,
+      '2024-06-10 10:30:00',
+      CURRENT_TIMESTAMP,
+      CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.leilao VALUES (
+      DEFAULT,
+      505,
+      'Leilão de vinhos finos',
+      6,
+      '2024-06-15 18:00:00',
+      CURRENT_TIMESTAMP,
+      CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.leilao VALUES (
+      DEFAULT,
+      606,
+      'Leilão de antiguidades',
+      7,
+      '2024-06-20 11:00:00',
+      CURRENT_TIMESTAMP,
+      CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.leilao VALUES (
+      DEFAULT,
+      707,
+      'Leilão de relógios de luxo',
+      8,
+      '2024-06-25 10:00:00',
+      CURRENT_TIMESTAMP,
+      CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.leilao VALUES (
+      DEFAULT,
+      808,
+      'Leilão de itens colecionáveis',
+      9,
+      '2024-07-01 14:00:00',
+      CURRENT_TIMESTAMP,
+      CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.leilao VALUES (
+      DEFAULT,
+      909,
+      'Leilão de objetos de decoração vintage',
+      10,
+      '2024-07-10 09:30:00',
+      CURRENT_TIMESTAMP,
+      CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.leilao VALUES (
+      DEFAULT,
+      1010,
+      'Leilão de livros raros',
+      11,
+      '2024-07-15 15:00:00',
+      CURRENT_TIMESTAMP,
+      CURRENT_TIMESTAMP
+);
+
+-- Dados na tabela Comprador
+INSERT INTO projeto.comprador VALUES (1,1);
+INSERT INTO projeto.comprador VALUES (2, 2);
+INSERT INTO projeto.comprador VALUES (3, 3);
+INSERT INTO projeto.comprador VALUES (4, 4);
+INSERT INTO projeto.comprador VALUES (5, 5);
+INSERT INTO projeto.comprador VALUES (6, 6);
+INSERT INTO projeto.comprador VALUES (7,8);
+INSERT INTO projeto.comprador VALUES (8, 7);
+INSERT INTO projeto.comprador VALUES (9, 10);
+INSERT INTO projeto.comprador VALUES (10, 9);
+
+-- Dados na tabela Unidade
+INSERT INTO projeto.unidade VALUES (
+       DEFAULT,
+       'Unidade A',
+       CURRENT_TIMESTAMP,
+       CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.unidade VALUES (
+       DEFAULT,
+       'Unidade B',
+       CURRENT_TIMESTAMP,
+       CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.unidade VALUES (
+       DEFAULT,
+       'Unidade C',
+       CURRENT_TIMESTAMP,
+       CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.unidade VALUES (
+       DEFAULT,
+       'Unidade D',
+       CURRENT_TIMESTAMP,
+       CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.unidade VALUES (
+       DEFAULT,
+       'Unidade E',
+       CURRENT_TIMESTAMP,
+       CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.unidade VALUES (
+       DEFAULT,
+       'Unidade F',
+       CURRENT_TIMESTAMP,
+       CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.unidade VALUES (
+       DEFAULT,
+       'Unidade G',
+       CURRENT_TIMESTAMP,
+       CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.unidade VALUES (
+       DEFAULT,
+       'Unidade H',
+       CURRENT_TIMESTAMP,
+       CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.unidade VALUES (
+       DEFAULT,
+       'Unidade I',
+       CURRENT_TIMESTAMP,
+       CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.unidade VALUES (
+       DEFAULT,
+       'Unidade J',
+       CURRENT_TIMESTAMP,
+       CURRENT_TIMESTAMP
+);
+
+-- Dados na tabela Lote
+INSERT INTO projeto.lote VALUES (
+        DEFAULT,
+        101,
+        'Lote de quadros',
+        5,
+        8000.00,
+        'Unidade B',
+        1,
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.lote VALUES (
+        DEFAULT,
+        202,
+        'Lote de relógios vintage',
+        8,
+        6000.00,
+        'Unidade C',
+        2,
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.lote VALUES (
+        DEFAULT,
+        303,
+        'Lote de moedas antigas',
+        15,
+        3000.00,
+        'Unidade D',
+        3,
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.lote VALUES (
+        DEFAULT,
+        404,
+        'Lote de selos raros',
+        20,
+        4000.00,
+        'Unidade E',
+        5,
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.lote VALUES (
+        DEFAULT,
+        505,
+        'Lote de móveis de época',
+        12,
+        7000.00,
+        'Unidade F',
+        6,
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.lote VALUES (
+        DEFAULT,
+        606,
+        'Lote de vinis clássicos',
+        30,
+        2000.00,
+        'Unidade G',
+        7,
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.lote VALUES (
+        DEFAULT,
+        707,
+        'Lote de livros raros',
+        25,
+        5000.00,
+        'Unidade H',
+        8,
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.lote VALUES (
+        DEFAULT,
+        808,
+        'Lote de esculturas',
+        6,
+        9000.00,
+        'Unidade I',
+        9,
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.lote VALUES (
+        DEFAULT,
+        909,
+        'Lote de instrumentos musicais antigos',
+        10,
+        7500.00,
+        'Unidade J',
+        10,
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+);
+
+INSERT INTO projeto.lote VALUES (
+        DEFAULT,
+        1010,
+        'Lote de porcelanas',
+        18,
+        3500.00,
+        'Unidade A',
+        4,
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+);
